6f0290c3e89d32f6e8b46266bf2c84968257b5f8
spyros1973 <Spyros@STABLE-SPYROS>
2016-10-16 12:30:51 +0300

    First commit for Assignment

---
+++ mod1-solution/App.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+        $scope.dishes = "";
+        $scope.message = "";
+        $scope.messageBorderColor = "inherit";
+        $scope.messageFontColor = "inherit";
+        $scope.checkDishes = function () {
+            if ($scope.dishes == '') {
+                $scope.message = "Please enter data first";
+                $scope.messageBorderColor = "red";
+                $scope.messageFontColor = "red";
+            } else if ($scope.message.split(",").length > 3) {
+                $scope.message = "Too much!";
+                $scope.messageBorderColor = "green";
+                $scope.messageFontColor = "green";
+            } else {
+                $scope.message = "Enjoy!";
+                $scope.messageBorderColor = "green";
+                $scope.messageFontColor = "green";
+            }
+        }
+    }
+
+})();
